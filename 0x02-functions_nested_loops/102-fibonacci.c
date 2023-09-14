74f974f351a88b0f261e978d831b4530cd0a7aa6
Sohaib <sohaibmahmoud2000@gmail.com>
2023-09-13 18:34:45 -0700

    Commit

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
+/**
+ * main - print 50 fibonacci numbers
+ *
+ * Return: (0)
+ */
+int main(void)
+{
+	long fb1, fb2, sum, counter;
 
-int main() {
-    int n = 50; // Number of Fibonacci numbers to generate
-    long long fib[n]; // Array to store Fibonacci numbers
-    
-    // Initialize the first two Fibonacci numbers
-    fib[0] = 1;
-    fib[1] = 2;
-
-    // Generate and store the remaining Fibonacci numbers
-    for (int i = 2; i < n; i++) {
-        fib[i] = fib[i - 1] + fib[i - 2];
-    }
-
-    // Print the Fibonacci numbers separated by commas and spaces
-    for (int i = 0; i < n - 1; i++) {
-        printf("%lld, ", fib[i]);
-    }
-
-    // Print the last Fibonacci number followed by a new line
-    printf("%lld\n", fib[n - 1]);
-
-    return 0;
+	fb1 = 0;
+	fb2 = 1;
+	for (counter = 0; counter < 50; counter++)
+	{
+		sum = fb1 + fb2;
+		fb1 = fb2;
+		fb2 = sum;
+		if (counter != 49)
+			printf("%ld, ", sum);
+		else
+			printf("%ld\n", sum);
+	}
+	return (0);
 }
